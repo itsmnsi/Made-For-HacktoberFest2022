0f349fba0643bb23fde7f809157283c2eeb56594
Omkar Sonawane <101235921+Omkar071011@users.noreply.github.com>
2022-10-19 00:17:37 +0530

    Update Factorial.cpp

--- CPP/Factorial.cpp
+++ CPP/Factorial.cpp
@@ -1,21 +1,24 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
 
-int main() {
-    int n;
-    long factorial = 1.0;
-
-    cout << "Enter a positive integer: ";
-    cin >> n;
-
-    if (n < 0)
-        cout << "Error! Factorial of a negative number doesn't exist.";
-    else {
-        for(int i = 1; i <= n; ++i) {
-            factorial *= i;
-        }
-        cout << "Factorial of " << n << " = " << factorial;    
-    }
-
-    return 0;
+int factorial(int n) {
+  if(n > 1)
+    return n * factorial(n - 1);
+  else
+    return 1;
 }
+
+
+
+int main() {
+
+  int n;
+
+  cout << "Enter a positive integer: ";
+  cin >> n;
+
+  cout << "Factorial is " << factorial(n);
+
+  return 0;
+}
+
